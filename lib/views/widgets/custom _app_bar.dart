3a437f0b7e895611c69cc5a3512f933463c414c4
Asmaa12300 <asmaaelneanaey1@gmail.com>
2026-04-25 20:02:26 +0300

    create notes list items

---
+++ lib/views/widgets/custom _app_bar.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @ovustom
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
